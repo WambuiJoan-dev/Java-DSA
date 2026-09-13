39fceb2f246a786a1989308f03d29d05d23d3a9c
+ <wambuijoan44@gmail.com>
2026-09-13 21:34:06 +0300

    addscoments

--- Duplicate.java
+++ Duplicate.java
@@ -2,13 +2,16 @@ import java.util.HashSet;
 
 class Duplicate{
     public boolean hasDuplicate(int[] nums) {
+        //create HashSet to store elements from the array
         HashSet<Integer> seenNumbers = new HashSet<>();
+        //iterate through each element in the array
         for (int num:nums)  {
+            //check if element is already in the HashSet
             if (seenNumbers.contains(num)) {
-                return true;
+                return true; //duplicate found
             }
-            seenNumbers.add(num);
+            seenNumbers.add(num); //add the element to the HashSet
         }
-        return false;
+        return false;//no duplicates found
     }
 }
