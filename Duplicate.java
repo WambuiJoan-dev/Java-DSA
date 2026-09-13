38e789e78146fcdef41adcca8405ada064fbe33d
+ <+>
2026-09-13 21:06:42 +0300

    learnsshashset

---
+++ Duplicate.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+class Duplicate{
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> seenNumbers = new HashSet<>();
+        for (int num:nums)  {
+            if (seenNumbers.contains(num)) {
+                return true;
+            }
+            seenNumbers.add(num);
+        }
+        return false;
+    }
+}
