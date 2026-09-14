7dee160b25be3107e2403d9abf8b9380ad4f86ad
+ <wambuijoan44@gmail.com>
2026-09-14 22:42:07 +0300

    ispalindrome

--- palindrome.java
+++ palindrome.java
@@ -6,7 +6,14 @@ class duplicate{
             while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
                 left++;
             }
-            while(left)
+            while(left<right &&!Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
+            left++;
+            right--;
         }
+        return true;
     }
-}~~
