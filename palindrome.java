5a111d4cbf4e9e33410cba10951865643f5749b0
+ <wambuijoan44@gmail.com>
2026-09-14 22:38:14 +0300

    ispalindrome

---
+++ palindrome.java
@@ -0,0 +1,12 @@
+class duplicate{
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length()-1;
+        while(left<right){
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+            while(left)
+        }
+    }
+}~~
