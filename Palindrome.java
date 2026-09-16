0242f94c282261b08200bc4b0bf7a433adf19d82
+ <wambuijoan44@gmail.com>
2026-09-16 23:21:20 +0300

    palindromcomments

--- Palindrome.java
+++ Palindrome.java
@@ -1,20 +1,20 @@
 class Palindrome{
     public boolean isPalindrome (String s) {
-        //initialising one pointer to the left and one to the right
+        //initialising one pointer to the left and one to the right of the string
         int left = 0;
         int right = s.length() - 1;
 
-        while(left<right) {
+        while(left<right) { //run a loop while left is less than right
             while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
-                left++;
+                left++; //if value of left side is not alphanumeric then update the left pointer(increase)
             }
             while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
-                right--;
+                right--; //if value of left side is not alphanumeric then update the left pointer(reduce)
             }
-            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
-                return false;
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){//check if they are same values
+                return false; //return false if they're not matching after converting to lowercase
             }
-            left++;
+            left++; //update values of left and right pointer
             right--;
         }
         return true;
