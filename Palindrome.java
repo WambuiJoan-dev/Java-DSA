36ebdd7aa478fc39d7016fd011e2a89f61502efa
+ <wambuijoan44@gmail.com>
2026-09-16 23:14:38 +0300

    palindrom

--- Palindrome.java
+++ Palindrome.java
@@ -1,12 +1,14 @@
-class duplicate{
-    public boolean isPalindrome(String s) {
+class Palindrome{
+    public boolean isPalindrome (String s) {
+        //initialising one pointer to the left and one to the right
         int left = 0;
-        int right = s.length()-1;
-        while(left<right){
+        int right = s.length() - 1;
+
+        while(left<right) {
             while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
                 left++;
             }
-            while(left<right &&!Character.isLetterOrDigit(s.charAt(right))){
+            while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
                 right--;
             }
             if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
@@ -17,4 +19,7 @@ class duplicate{
         }
         return true;
     }
+}
+
+
     //Time O(n) Space 0(1)
